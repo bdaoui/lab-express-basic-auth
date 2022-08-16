2f847c677bee81c198f5214febf9270c98966dfb
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-08-16 22:32:47 +0200

    The Iteration 1 is done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const mongoose = require("mongoose");
 const User = require("../models/User.model");
 const bcrypt = require("bcrypt");
 const saltRounds = 1;
@@ -8,15 +7,32 @@ const router = express.Router();
 
 
 
-router.get("auth/signup", (req, res)=> {
-    res.render("signup");
+router.get("/signup", (req, res)=> {
+    res.render("auth/signup");
+})
+
+router.get("/profile", (req, res) =>{
+    res.render("auth/profile");
 })
 
 
 router.post("/signup", (req, res)=>{
     const dataUser = req.body;
 
-})
+    bcrypt
+        .genSalt(saltRounds)
+        .then(salt => bcrypt.hash(dataUser.password, salt))
+        .then(hashPassword =>{ 
+            return User.create({
+                "username": dataUser.name,
+                "password": hashPassword,
+            });
+        })
+        .then(result =>{
+            res.render("auth/profile", {result})
+        })
+        .catch(error => console.log("this is the issue: ", error));
+})  ;
 
 
 module.exports = router;
